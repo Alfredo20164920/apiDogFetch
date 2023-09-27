aaa07c0b01abe534e11c9be7d33014acf73672b4
Alfredo20164920 <apaz20164920@gmail.com>
2023-09-27 12:51:41 -0600

    feat: render data

--- src/components/gridCards/index.tsx
+++ src/components/gridCards/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Container } from './styled'
 // import AuthContext from '../../context/auth';
 import { api } from '../../API/index';
@@ -6,24 +6,34 @@ import FilterContext from '../../context/filter';
 
 export interface IFilterParams {
   breeds?: string[],
+}
 
+export interface Dog {
+  id: string
+  img: string
+  name: string
+  age: number
+  zip_code: string
+  breed: string
 }
 
 const Component = () => {
 
+  const [data, setdata] = useState<Dog[]>([])
+
   const {filterDogs} = useContext(FilterContext);
-  console.log(filterDogs);
   useEffect(() => {
     api.post("/dogs", filterDogs, {withCredentials: true})
       .then(res => {
-        console.log(res.data)
+        const {data} = res;
+        setdata(data);
       })
       .catch(e => console.log(e))
   }, [filterDogs])
 
   return (
     <Container>
-      grid
+      {data.map((item, index: number) => <p key={index}>{item?.breed}</p>)}
     </Container>
   )
 }
