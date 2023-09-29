8476843e0f5b4ef36679f6be08695147378cce7e
Alfredo20164920 <apaz20164920@gmail.com>
2023-09-28 21:53:19 -0600

    feat: implemtent localstorage and logout
    - Implemente hook to save in localStorage
    - Create method to logout

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,45 +1,38 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext, useState } from 'react';
+import { createContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import { IAuthContext, ILogin } from '../types/index';
 import { api } from '../API/index';
+import { LOCAL_STORAGE_KEYS, useLocalStorage } from '../hooks/useLocalStorage';
 
 export const AuthContext = createContext<IAuthContext>({user: null, login: () => {}, logout: () => {}});
 
 export const AuthContextProvider = ({children}: any) => {
 
-  const [user, setUser] = useState(() => {
-    const userProfle = localStorage.getItem("userProfile");
-    if (userProfle) {
-      const res = JSON.parse(userProfle);
-      return res;
-    }
-    return null;
-  });
-
+  const [storedValue, setValue, removeValue] = useLocalStorage(LOCAL_STORAGE_KEYS.AUTH_PROFILE, {});
   const navigate = useNavigate();
 
   const login = async (payload: ILogin) => {
     const storage = {
       200: 'ok',
+      user: payload.name,
       createAt: new Date()
     };
     await api.post("/auth/login", payload, {withCredentials: true});
-    localStorage.setItem("userProfile", JSON.stringify(storage));
-    setUser(storage)
+    setValue(storage);
     navigate('/');
   }
 
   const logout = async () => {
     await api.post('/auth/logout', null, {withCredentials: true})
-    localStorage.removeItem('userProfile');
+    removeValue();
     navigate('/login');
   }
 
   return (
     <>
-      <AuthContext.Provider value={{user, login, logout}}>
+      <AuthContext.Provider value={{user: storedValue, login, logout}}>
         {children}
       </AuthContext.Provider>
     </>
